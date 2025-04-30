78a6ff756cd880ff1f9c47cd0582a199147160de
pg964982@gmail.com <pg964982@gmail.com>
2025-04-30 15:54:26 -0300

    Validação dos campos do produto

--- MauiAppMinhasCompras/Moldes/Produto.cs
+++ MauiAppMinhasCompras/Moldes/Produto.cs
@@ -4,10 +4,33 @@ namespace MauiAppMinhasCompras.Moldes
 {
     public class Produto
     {
+        string _descricao;
+        double _qtn;
+
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
-        public string Descricao { get; set; }
-        public double Quantidade { get; set; }
+        public string Descricao {
+            get => _descricao;
+            set 
+            {
+                if (value == null) 
+                {
+                    throw new Exception("Por favor preencha a descrição");
+                }
+                _descricao = value;
+            } 
+        }
+        public double Quantidade {
+            get => _qtn;
+            set
+            {
+                if (value == 0)
+                {
+                    throw new Exception("Quantidade tem que ser maior que 0");
+                }
+                _qtn = value;
+            }
+             }
         public double Preco { get; set; }
         public double Total { get => Quantidade * Preco; }
     }
